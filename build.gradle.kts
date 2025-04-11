e502090f72cdfb6f07febf95b9f498bbd984db00
HusinHakim <husinhidayatul@gmail.com>
2025-04-11 10:24:51 +0700

    [FEAT]: Initialize Spring Boot Project

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    java
+    jacoco
+    id("org.sonarqube") version "6.0.1.5171"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "auth-service-rizzerve")
+        property("sonar.projectName", "auth-service-rizzerve")
+    }
+}
+
+group = "rizzerve"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+} 
